418fa389f1eeec6bd724b66d3a99fff620771a50
Juan Toledo <toledo+github@lazaro.es>
2017-07-22 23:53:16 +0200

    Separated Canvas2D from the CarSim app

--- CarSim.py
+++ CarSim.py
@@ -1,78 +1,25 @@
-from math import pi, sin, cos
+from math import pi
 import numpy as np
 
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from kivy.properties import NumericProperty, ObjectProperty
 
-view_matrix = ObjectProperty(np.identity(3))
+from Canvas2D import Polygon
 
 
-# Using this for refence on transformations
-# https://www.gamedev.net/articles/programming/math-and-physics/making-a-game-engine-transformations-r3566/
+class Car(Polygon):
 
-
-class Canvas2D(Widget):
-    """Root Canvas on wich other Canvas2D elements are placed
-    It supports viewport and element transformations"""
-
-    def __init__(self, **kwargs):
-        super(Canvas2D, self).__init__(**kwargs)
-        self.view_matrix = np.identity(3)
-
-
-class Polygon(Widget):
-
-    # Properties don't work well with Numpy arrays
-    coords = ObjectProperty((50, 50), force_dispatch=True)
     heading = NumericProperty(0)
-    rotation = NumericProperty(0.0)
-
-    points = ObjectProperty((-9, -20, 9, -20, 9, 20, 9, -20))
-    view_points = ObjectProperty((0, 0))
-
     steering = NumericProperty(0.0)
 
-    def __init__(self, **kwargs):
-        super(Polygon, self).__init__(**kwargs)
-        self.matrix = np.identity(3)
-        self.update_view_points()
-
-    def update_view_points(self):
-        self.matrix = np.matrix(
-            ((1, 0, self.coords[0]),
-             (0, 1, self.coords[1]), (0, 0, 1))) * np.array(
-            ((cos(self.rotation), -sin(self.rotation), 0),
-             (sin(self.rotation), cos(self.rotation), 0),
-             (0, 0, 1)))
-        self.view_points = self.transform(self.points)
-
-    def transform(self, points):
-        # De manera general es mejor utilizar un yield, supongo
-        points = zip(points[0::2], points[1::2], [1] * int((len(points) / 2)))
-        res = []
-        for p in points:
-            p = self.matrix * np.transpose(np.matrix(p))
-            x, y = p[0, 0], p[1, 0]
-            res += (x, y)
-        return res
-
     def move(self, d):
-        # self.coords = self.coords + np.array((d, d))
-        # self.heading = self.heading + d
-        self.rotation = self.rotation + d * pi / 180
-        pass
-
-    def on_coords(self, widget, coords):
-        self.update_view_points()
+        self.coords = self.coords + np.array((d, d))
+        self.heading = self.heading + d
 
     def on_heading(self, widget, heading):
         self.heading = self.heading % 360
         self.rotation = - self.heading * pi / 180
-        self.update_view_points()
-
-    def on_rotation(self, widget, rotation):
         self.update_view_points()
 
 
@@ -85,7 +32,7 @@ class CarSimApp(App):
 
     def on_start(self, **kwargs):
         Clock.schedule_interval(self.timer, 0.3)
-        self.car = Polygon()
+        self.car = Car()
         self.root.add_widget(self.car)
 
 
