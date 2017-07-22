2af4b05c9d6afe557fe2780c1c5c5190dfeb2c77
Juan Toledo <toledo+github@lazaro.es>
2017-07-23 00:12:17 +0200

    Generalized Widget2D wich will update its transformation matrix according to its parent

--- Canvas2D.py
+++ Canvas2D.py
@@ -17,11 +17,32 @@ class Canvas2D(Widget):
         self.view_matrix = np.identity(3)
 
 
-class Polygon(Widget):
+class Widget2D(Widget):
 
     # Properties don't work well with Numpy arrays
-    coords = ObjectProperty((50, 50), force_dispatch=True)
+    coords = ObjectProperty((0, 0), force_dispatch=True)
     rotation = NumericProperty(0.0)
+
+    def __init__(self, **kwargs):
+        super(Widget2D, self).__init__(**kwargs)
+        self.update_matrix()
+
+    def update_matrix(self):
+        self.matrix = np.matrix(
+            ((1, 0, self.coords[0]),
+             (0, 1, self.coords[1]), (0, 0, 1))) * np.array(
+            ((cos(self.rotation), -sin(self.rotation), 0),
+             (sin(self.rotation), cos(self.rotation), 0),
+             (0, 0, 1)))
+
+    def on_coords(self, widget, coords):
+        self.update_matrix()
+
+    def on_rotation(self, widget, rotation):
+        self.update_matrix()
+
+
+class Polygon(Widget2D):
 
     points = ObjectProperty(())
     view_points = ObjectProperty((0, 0))
@@ -31,12 +52,7 @@ class Polygon(Widget):
         self.update_view_points()
 
     def update_view_points(self):
-        self.matrix = np.matrix(
-            ((1, 0, self.coords[0]),
-             (0, 1, self.coords[1]), (0, 0, 1))) * np.array(
-            ((cos(self.rotation), -sin(self.rotation), 0),
-             (sin(self.rotation), cos(self.rotation), 0),
-             (0, 0, 1)))
+        self.update_matrix()
         self.view_points = self.transform(self.points)
 
     def transform(self, points):
@@ -50,7 +66,9 @@ class Polygon(Widget):
         return res
 
     def on_coords(self, widget, coords):
+        super(Polygon, self).on_coords(widget, coords)
         self.update_view_points()
 
     def on_rotation(self, widget, rotation):
+        super(Polygon, self).on_rotation(widget, rotation)
         self.update_view_points()
